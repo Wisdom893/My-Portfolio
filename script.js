9fb1618e58d7c7ead94db89096694dd3cbbeff7f
Wisdom893 <wxichavo@gmail.com>
2025-11-23 10:18:47 +0200

    changed some comments

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const toggleBtn = document.getElementById("toggle-theme");
 const body = document.body;
 
-// Load saved theme from localStorage
+
 if (localStorage.getItem("theme") === "dark") {
   body.classList.add("dark-mode");
   toggleBtn.textContent = "☀️ Light Mode";
